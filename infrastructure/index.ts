8c609138d59acbc8f51b86f06190f267b066a300
Rafael Ortiz <rd.ortizr1@uniandes.edu.co>
2023-09-17 10:11:11 -0500

    [Feature] Add test infrastructure as code

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,3 +1,1 @@
-import * as k8s from './k8s/cluster';
-
-export const kubeconfig = k8s.kubeconfig;
+export * from './k8s/cluster';
--- infrastructure/k8s/cluster.ts
+++ infrastructure/k8s/cluster.ts
@@ -1,33 +1,28 @@
-import * as k8s from "@pulumi/kubernetes";
-import * as pulumi from "@pulumi/pulumi";
-import * as gcp from "@pulumi/gcp";
+import * as gcp from '@pulumi/gcp';
+import * as pulumi from '@pulumi/pulumi';
+import * as k8s from '@pulumi/kubernetes';
 
-const name = "experiment";
+const config = new pulumi.Config();
+const environment = pulumi.getStack();
+export const clusterName = `${config.require('gcp:k8s-cluster-name')}-${environment}`;
+
 
 // Create a GKE cluster
 const engineVersion = gcp.container.getEngineVersions().then(v => v.latestMasterVersion);
 
-const cluster = new gcp.container.Cluster(name, {
-  initialNodeCount: 2,
+
+const cluster = new gcp.container.Cluster(clusterName, {
+  enableAutopilot: true,
+  location: "us-central1",
+  name: clusterName,
   minMasterVersion: engineVersion,
-  nodeVersion: engineVersion,
-  nodeConfig: {
-    machineType: "n1-standard-1",
-    oauthScopes: [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring"
-    ],
+  releaseChannel: {
+    channel: 'STABLE',
   },
+}, {
+  ignoreChanges: ['verticalPodAutoscaling'] // beacuse we are using autopilot verticalPodAutoscaling is handle by the GCP
 });
 
-// Export the Cluster name
-export const clusterName = cluster.name;
-
-// Manufacture a GKE-style kubeconfig. Note that this is slightly "different"
-// because of the way GKE requires gcloud to be in the picture for cluster
-// authentication (rather than using the client cert/key directly).
 export const kubeconfig = pulumi.
   all([cluster.name, cluster.endpoint, cluster.masterAuth]).
   apply(([name, endpoint, masterAuth]) => {
@@ -49,16 +44,16 @@ preferences: {}
 users:
 - name: ${context}
   user:
-    exec:
-      apiVersion: client.authentication.k8s.io/v1beta1
-      command: gke-gcloud-auth-plugin
-      installHint: Install gke-gcloud-auth-plugin for use with kubectl by following
-        https://cloud.google.com/blog/products/containers-kubernetes/kubectl-auth-changes-in-gke
-      provideClusterInfo: true
+    auth-provider:
+      config:
+        cmd-args: config config-helper --format=json
+        cmd-path: gcloud
+        expiry-key: '{.credential.token_expiry}'
+        token-key: '{.credential.access_token}'
+      name: gcp
 `;
   });
 
-// Create a Kubernetes provider instance that uses our cluster from above.
-const clusterProvider = new k8s.Provider(name, {
-  kubeconfig,
+export const clusterProvider = new k8s.Provider(clusterName, {
+  kubeconfig: kubeconfig,
 });
